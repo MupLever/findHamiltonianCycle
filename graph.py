cc698a9efa356b32a8ba165353713b179dbb862f
shlyzhas <8willox8@gmail.com>
2023-10-10 22:50:49 +0300

    прошелся линтером и справил ворнинги

--- graph.py
+++ graph.py
@@ -1,66 +1,77 @@
 from typing import List
-from collections import defaultdict
-
-class Node:
-    def __init__(self, value: tuple):
-        self.value = value
-        self.edges = set()
-        # self.parents = {}
-
-
-class Edge:
-    def __init__(self, adjacentNode: Node, weight: int):
-        self.adjacentNode = adjacentNode
-        self.weight = weight
-
+# from collections import defaultdict
 
 class Graph:
-    def __init__(self, input: List[list]):
+    """an undirected graph class implemented through a dictionary"""
+
+    class Node:
+        """node class storing node value and incident edges"""
+
+        def __init__(self, value: int):
+            self.value = value
+            self.edges = set()
+            # self.parents = {}
+
+    class Edge:
+        """edge class storing the incident node"""
+
+        def __init__(self, adjacent_node, weight: int):
+            self.adjacent_node = adjacent_node
+            self.weight = weight
+
+    def __init__(self, data_input: List[list]):
         self.graph = {}
 
-        for from_, to_, weight in input:
+        for from_, to_, weight in data_input:
             node = self.add_or_get_node(from_)
-            adjacentNode = self.add_or_get_node(to_)
+            adjacent_node = self.add_or_get_node(to_)
 
-            edge = Edge(adjacentNode, weight)
+            edge = self.Edge(adjacent_node, weight)
             node.edges.add(edge)
 
-            edge = Edge(node, weight)
-            adjacentNode.edges.add(edge)
+            edge = self.Edge(node, weight)
+            adjacent_node.edges.add(edge)
 
-    def add_or_get_node(self, value):
+    def add_or_get_node(self, value: int) -> None:
+        """adding and returning a node"""
+
         if value not in self.graph:
-            self.graph[value] = Node(value)
+            self.graph[value] = self.Node(value)
         return self.graph[value]
 
-    def dfs(self):
-        passed = set()
-        for node_value in self.graph:
-            if node_value not in passed:
-                self._dfs_without_recur(self.graph[node_value], passed)
+    def dfs(self) -> None:
+        """depth-first traversal of all nodes"""
 
-    def _dfs_without_recur(self, node: Node, passed: set):
+        passed = set()
+        for value, node in self.graph.items():
+            if value not in passed:
+                self._dfs_without_recur(node, passed)
+
+    def _dfs_without_recur(self, node: Node, passed: set) -> None:
+        """depth-first traversal without recursion"""
+
         stack = [node.value]
         while stack:
             node_value = stack[-1]
             if node_value not in passed:
                 print(node_value)
                 passed.add(node_value)
-            hasChildren = False
+            has_children = False
             for edge in self.graph[node_value].edges:
-                if edge.adjacentNode.value not in passed:
-                    stack.append(edge.adjacentNode.value)
-                    hasChildren = True
+                if edge.adjacent_node.value not in passed:
+                    stack.append(edge.adjacent_node.value)
+                    has_children = True
                     break
-            if not hasChildren:
+            if not has_children:
                 stack.pop()
 
-    def _dfs_with_recur(self, node: Node, passed: set):
+    def _dfs_with_recur(self, node: Node, passed: set) -> None:
+        """depth-first traversal with recursion"""
         print(node.value)
         passed.add(node.value)
         for edge in node.edges:
-            if edge.adjacentNode.value not in passed:
-                self._dfs_with_recur(edge.adjacentNode, passed)
+            if edge.adjacent_node.value not in passed:
+                self._dfs_with_recur(edge.adjacent_node, passed)
 
 data = \
 [
