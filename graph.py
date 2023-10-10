1f4c68087caa7f211b29463ae5deb2b08fe079d7
shlyzhas <8willox8@gmail.com>
2023-10-10 21:39:16 +0300

    создание графа и поиск в глубину

--- graph.py
+++ graph.py
@@ -1,11 +1,12 @@
 from typing import List
-
+from collections import defaultdict
 
 class Node:
-    def __init__(self, value: int):
+    def __init__(self, value: tuple):
         self.value = value
         self.edges = set()
-        self.parents = {}
+        # self.parents = {}
+
 
 class Edge:
     def __init__(self, adjacentNode: Node, weight: int):
@@ -14,13 +15,48 @@ class Edge:
 
 
 class Graph:
-    def __init__(self, input: List[List[int]]):
-        for from_, to_, weight in input:
-            pass
+    def __init__(self, input: List[list]):
+        self.graph = {}
 
-    def add_edge(self, value: int):
-        Node(value)
-        pass
+        for from_, to_, weight in input:
+            node = self.add_or_get_node(from_)
+            adjacentNode = self.add_or_get_node(to_)
+
+            edge = Edge(adjacentNode, weight)
+            node.edges.add(edge)
+
+            edge = Edge(node, weight)
+            adjacentNode.edges.add(edge)
+
+    def add_or_get_node(self, value):
+        if value not in self.graph:
+            self.graph[value] = Node(value)
+        return self.graph[value]
     
-    def add_node(self):
-        pass
+    def dfs(self):
+        passed = set()
+        for node in self.graph:
+            if node not in passed:
+                passed.add(node)
+                self._dfs_inner(self.graph[node], passed)
+
+    def _dfs_inner(self, node: Node, passed: set):
+        print(node.value)
+        passed.add(node.value)
+        for edge in node.edges:
+            if edge.adjacentNode.value not in passed:
+                self._dfs_inner(edge.adjacentNode, passed)
+
+data = \
+[
+    [7, 6, 1],
+    [7, 2, 1],
+    [7, 5, 1],
+    [6, 4, 1],
+    [2, 1, 1],
+    [5, 9, 1],
+    [8, 10, 1]
+]
+
+graph = Graph(data)
+graph.dfs()
