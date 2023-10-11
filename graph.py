5f6af7dfac8baa8961d099e9b58ac01fe641f40b
shlyzhas <8willox8@gmail.com>
2023-10-11 21:55:09 +0300

    метод поиска субоптимального гамильтонова цикла

--- graph.py
+++ graph.py
@@ -1,5 +1,6 @@
 from typing import List
 from queue import Queue
+# from random import choice
 
 
 class Graph:
@@ -25,6 +26,9 @@ class Graph:
         def __init__(self, adjacent_node, weight: int):
             self.adjacent_node = adjacent_node
             self.weight = weight
+
+        def __lt__(self, other):
+            return self.weight < other.weight
 
     def __init__(self, data_input: List[list]):
         self.graph = {}
@@ -53,7 +57,11 @@ class Graph:
         from_node.edges.add(edge)
 
     def traverse(self, *, how="dfs") -> None:
-        """depth-first traversal of all nodes"""
+        """
+        depth-first traversal of all nodes
+        :type how: str
+        :values how: 'bfs', 'dfs', 'rdfs'
+        """
 
         if how == "bfs":
             traverse_ = self._bfs
@@ -62,7 +70,7 @@ class Graph:
         elif how == "rdfs":
             traverse_ = self._dfs_without_recur
         else:
-            raise ValueError
+            raise ValueError("invalid argument value")
 
         passed = set()
         for value, node in self.graph.items():
@@ -110,8 +118,8 @@ class Graph:
             if edge.adjacent_node.value not in passed:
                 self._dfs_with_recur(edge.adjacent_node, passed)
 
-    def dirac_theorem(self) -> bool:
-        """"""
+    def _dirac_theorem(self) -> bool:
+        """verifies Dirac's theorem"""
 
         if self.n_vertex < 3:
             return False
@@ -119,23 +127,62 @@ class Graph:
         for value1, node1 in self.graph.items():
             for value2, node2 in self.graph.items():
                 if value1 != value2:
-                    incident_nodes = map(lambda edge: edge.adjacent_node, node2.edges)
-                    if node1 not in incident_nodes:
+                    incidental_nodes = map(lambda edge: edge.adjacent_node,
+                                           node2.edges)
+                    if node1 not in incidental_nodes:
                         if len(node1.edges) + len(node2.edges) < self.n_vertex:
                             return False
 
         return True
 
-data = [
-    [7, 6, 1],
-    [7, 2, 1],
-    [7, 5, 1],
-    [6, 4, 1],
-    [2, 1, 1],
-    [5, 9, 1],
-    [8, 10, 1]
-]
+    def _weight_between_nodes(self, node: Node, adjacent_node: Node) -> int:
+        """return weight between adjacent nodes"""
 
+        for edge in node.edges:
+            if edge.adjacent_node == adjacent_node:
+                return edge.weight
+
+        raise ValueError("nodes are not adjacent")
+
+    def find_hamiltonian_cycle(self) -> int:
+        """finds a suboptimal Hamiltonian cycle"""
+
+        if not self._dirac_theorem():
+            return 0
+
+        # start_node = choice(self.graph.values())
+        start_node_value = 'D'
+        route = f"{start_node_value}"
+        route_len = 0
+        cur_node_value = start_node_value
+        passed = set(cur_node_value)
+        while len(passed) != self.n_vertex:
+            edges_to_not_passed_nodes = filter(
+                lambda edge: edge.adjacent_node.value not in passed,
+                self.graph[cur_node_value].edges)
+
+            edge_to_nearest_node = min(edges_to_not_passed_nodes)
+            route_len += edge_to_nearest_node.weight
+            nearest_node_value = edge_to_nearest_node.adjacent_node.value
+            route += f"-> {nearest_node_value}"
+            passed.add(nearest_node_value)
+            cur_node_value = nearest_node_value
+        route += f"-> {start_node_value}"
+        route_len += self._weight_between_nodes(self.graph[start_node_value],
+                                                self.graph[cur_node_value])
+        print(route)
+        return route_len
+
+
+# data = [
+#     [7, 6, 1],
+#     [7, 2, 1],
+#     [7, 5, 1],
+#     [6, 4, 1],
+#     [2, 1, 1],
+#     [5, 9, 1],
+#     [8, 10, 1]
+# ]
 
 # data = [
 #     ['x1', 'x2', 1],
@@ -146,7 +193,15 @@ data = [
 #     ['x3', 'x5', 1],
 #     ['x4', 'x5', 1]
 # ]
+data = [
+    ['A', 'B', 5],
+    ['A', 'C', 6],
+    ['A', 'D', 8],
+    ['B', 'C', 7],
+    ['B', 'D', 10],
+    ['C', 'D', 3]
+]
 
 graph = Graph(data)
 # graph.traverse(how="bfs")
-print(graph.dirac_theorem())
+print(graph.find_hamiltonian_cycle())
